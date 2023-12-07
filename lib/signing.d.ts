8437901b095c95c82e147d8a25859b59c910acbb
deep-soft <deep-soft@users.noreply.github.com>
2023-12-07 21:56:04 +0200

    v1.1.0
    - @vercel/ncc
    - dist/index.js
    
    - npm install
    - npm i --save-dev @types/node
    - npm ci
    - npm audit fix
    - npm run build

---
+++ lib/signing.d.ts
@@ -0,0 +1,2 @@
+export declare function signApkFile(apkFile: string, signingKeyFile: string, alias: string, keyStorePassword: string, keyPassword?: string): Promise<string>;
+export declare function signAabFile(aabFile: string, signingKeyFile: string, alias: string, keyStorePassword: string, keyPassword?: string): Promise<string>;
