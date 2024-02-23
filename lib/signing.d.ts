c2c2f8eab786dec2e76f1c5ffcc25402808ff6d0
deep-soft <deep-soft@users.noreply.github.com>
2024-02-23 05:34:01 +0200

    Delete lib directory

--- lib/signing.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function signApkFile(apkFile: string, signingKeyFile: string, alias: string, keyStorePassword: string, keyPassword?: string): Promise<string>;
-export declare function signAabFile(aabFile: string, signingKeyFile: string, alias: string, keyStorePassword: string, keyPassword?: string): Promise<string>;
